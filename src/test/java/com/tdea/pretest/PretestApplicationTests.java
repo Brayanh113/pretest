2c28e0bb7a7da43894113079b96c93a12782f694
Brayan Hincapié Monsalve <88449950+Brayanh113@users.noreply.github.com>
2022-03-19 11:38:48 -0500

    Update PretestApplicationTests.java

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -2,10 +2,20 @@ package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class PretestApplicationTests {
 
     @Test
     void contextLoads() {
+        String  dbPassword ="123";
+        String password= "1234";
+
+        boolean result = UserUtils.isPasswordEqual(dbPassword, password);
+
+        assertFalse(result);
+
     }
 
 }
